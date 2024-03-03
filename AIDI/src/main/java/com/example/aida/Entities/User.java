1264a73bb463219a2a823c86bb8d62bae29ead8b
omnyaahmed292 <omnyaahmed292@gmail.com>
2024-03-03 16:04:35 +0200

    Make user image 1 to 1

--- AIDI/src/main/java/com/example/aida/Entities/User.java
+++ AIDI/src/main/java/com/example/aida/Entities/User.java
@@ -69,6 +69,10 @@ public class User {
     @Column(length = 10)
     private String addressBuildingNo;
 
+
+    @Column(length = 255)
+    private String image_file_path;
+
     @OneToMany(mappedBy = "customer")
     private Set<Customer> customerCustomers;
 
@@ -81,13 +85,7 @@ public class User {
     @OneToMany(mappedBy = "user")
     private Set<Card> userCards;
 
-    @ManyToMany
-    @JoinTable(
-            name = "UserImage",
-            joinColumns = @JoinColumn(name = "userId"),
-            inverseJoinColumns = @JoinColumn(name = "imageId")
-    )
-    private Set<Image> userImageImages;
+
 
     public Integer getUserId() {
         return userId;
@@ -209,14 +207,15 @@ public class User {
         this.userCards = userCards;
     }
 
-    public Set<Image> getUserImageImages() {
-        return userImageImages;
+    public void setImage_file_path(String image_file_path) {
+        this.image_file_path = image_file_path;
     }
 
 
-    public void setUserImageImages(final Set<Image> userImageImages) {
-        this.userImageImages = userImageImages;
+    public String getImage_file_path() {
+        return image_file_path;
     }
+
     public Collection<String> getRoles() {
 
         return Arrays.asList("ADMIN", "Customer","Vendor");
