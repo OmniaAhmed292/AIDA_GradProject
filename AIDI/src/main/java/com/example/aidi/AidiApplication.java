7f039f9aed10bb5c4592ccb10c5b4d78209f1d20
hanan saeed <hanansaeedbs12@gmail.com>
2024-03-03 12:06:12 +0200

    intial commit

---
+++ AIDI/src/main/java/com/example/aidi/AidiApplication.java
@@ -0,0 +1,13 @@
+package com.example.aidi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AidiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AidiApplication.class, args);
+    }
+
+}
---
+++ AIDI/src/test/java/com/example/aidi/AidiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aidi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AidiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
