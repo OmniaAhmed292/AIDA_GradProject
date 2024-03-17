2d1d9fa9b3ee2c090a50a0babf836656e0bd4b6b
omnyaahmed292 <omnyaahmed292@gmail.com>
2024-03-17 02:06:18 +0200

    Half Entities migration

---
+++ AIDI/src/main/java/com/example/aida/Entities/VendorSettings.java
@@ -0,0 +1,21 @@
+package com.example.aida.Entities;
+
+import jakarta.persistence.Embeddable;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Setter
+@Getter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@Embeddable
+public class VendorSettings {
+    @Field("allow_late_emails")
+    private boolean AllowLateEmails;
+
+    @Field("allow_new_emails")
+    private boolean AllowNewEmails;
+}
