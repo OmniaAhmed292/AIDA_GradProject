068c3981d47a64dfe578c31a295df13ed6a1d7a0
Sar00-Ahmed <sarasara6612@yahoo.com>
2024-05-02 18:58:04 +0300

    fixed env and product repository

--- AIDI/src/main/java/com/example/aida/Repositories/VendorRepository.java
+++ AIDI/src/main/java/com/example/aida/Repositories/VendorRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 @Transactional
 public interface VendorRepository extends MongoRepository<Vendor, String> {
     // Additional methods specific to Vendor (if needed)
+    <Optional> Vendor findById(String id);
 }
