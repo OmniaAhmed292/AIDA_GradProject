7e1d04ed4f8f6a29064125814e50684aaf6dd24e
Sar00-Ahmed <sarasara6612@yahoo.com>
2024-05-07 01:48:27 +0300

    customer and vendor read operations base

--- AIDI/src/main/java/com/example/aida/Repositories/CustomerRepository.java
+++ AIDI/src/main/java/com/example/aida/Repositories/CustomerRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 @Transactional
 public interface CustomerRepository extends MongoRepository<Customer, String> {
     <Optional> Customer findById(String id);
+    <Optional> Customer findByEmail(String email);
 }
---
+++ AIDI/src/main/java/com/example/aida/service/UsersService/VendorService.java
@@ -0,0 +1,4 @@
+package com.example.aida.service.UsersService;
+
+public class VendorService {
+}
