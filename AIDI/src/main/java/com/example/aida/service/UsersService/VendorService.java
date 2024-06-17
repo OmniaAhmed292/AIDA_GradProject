f3c608a3fae06387b4ecf980ec47faa2db7fd009
Sar00-Ahmed <sarasara6612@yahoo.com>
2024-06-17 20:33:14 +0300

    completed and fixed vendor and cutomer update and read operations:
    -update info
    -update cards
    -update settings

--- AIDI/src/main/java/com/example/aida/service/UsersService/VendorService.java
+++ AIDI/src/main/java/com/example/aida/service/UsersService/VendorService.java
@@ -1,4 +1,64 @@
 package com.example.aida.service.UsersService;
 
+import com.example.aida.Entities.Address;
+import com.example.aida.Entities.Card;
+import com.example.aida.Entities.Vendor;
+import com.example.aida.Entities.VendorSettings;
+import com.example.aida.Repositories.VendorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
 public class VendorService {
+    @Autowired
+    private VendorRepository vendorRepository;
+
+    public Vendor getVendorInfo(){
+        String username = getAuthenticatedUsername();
+        return vendorRepository.findByEmail(username);
+    }
+
+    public Vendor updateStoreInfo(String phoneNumber, Address address, String storeName){
+        Vendor vendor = getVendorInfo();
+        if (vendor != null) {
+            vendor.setPhoneNumber(phoneNumber);
+            vendor.setAddress(address);
+            vendor.getBusinessInfo().setBusinessName(storeName);
+            vendorRepository.save(vendor);
+        }
+        return vendor;
+    }
+
+    public Vendor updateCards(Set<Card> cards){
+        Vendor vendor = getVendorInfo();
+        if (vendor != null) {
+            vendor.setCards(cards);
+            vendorRepository.save(vendor);
+        }
+        return vendor;
+    }
+
+    public Vendor updateSettings(VendorSettings settings){
+        Vendor vendor = getVendorInfo();
+        if (vendor != null) {
+            vendor.setSettings(settings);
+            vendorRepository.save(vendor);
+        }
+        return vendor;
+    }
+
+    private String getAuthenticatedUsername() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails) {
+            return ((UserDetails)principal).getUsername();
+        } else {
+            return principal.toString();
+        }
+    }
 }
